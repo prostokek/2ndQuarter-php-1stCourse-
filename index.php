423408824bff2598b0e36b076d9d7338b65af6a0
prostokek <dimagalyanov@gmail.com>
2019-05-07 15:00:48 +0300

    В процессе

--- index.php
+++ index.php
@@ -1,113 +1,95 @@
 <?php 
-    $title = '2ndLesson';
-    $h1 = 'Второй урок';
+    $title = 'Третий урок';
+    $h1 = 'Третий урок';
 
     /* ПЕРВОЕ ЗАДАНИЕ */
 
-    $a = 10;
-    $b = 20;
-    if ($a >0 && $b > 0) {
-        $c = $a - $b;
-        $result_1 = "Разность a и b = $c"  ;
-    } elseif ($a < 0 && $b < 0) {
-        $c = $a * $b;
-        $result_1 = "Произведение a и b = $c";
-    } else {
-        $c = $a + $b;
-        $result_1 = "Сумма a и b = $c";
-    };
+    $i = 0;
+    $res_1 = "";
+    while ($i < 100) {
+        $i++;
+        if($i % 3 == 0) {
+            $res_1 .= ($i . " ");
+        }
+    }
 
     /* /ПЕРВОЕ ЗАДАНИЕ */
 
     /* ВТОРОЕ ЗАДАНИЕ */
-    $z = 7;
-    $result_2 = "";
-    switch ($z) {
-        case 1:
-            $result_2 .= "1 ";
-        case 2:
-            $result_2 .= "2 ";
-        case 3:
-            $result_2 .= "3 ";
-        case 4:
-            $result_2 .= "4 ";
-        case 5:
-            $result_2 .= "5 ";
-        case 6:
-            $result_2 .= "6 ";
-        case 7:
-            $result_2 .= "7 ";
-        case 8:
-            $result_2 .= "8 ";
-        case 9:
-            $result_2 .= "9 ";
-        case 10:
-            $result_2 .= "10 ";
-        case 11:
-            $result_2 .= "11 ";
-        case 12:
-            $result_2 .= "12 ";
-        case 13:
-            $result_2 .= "13 ";
-        case 14:
-            $result_2 .= "14 ";
-        case 15:
-            $result_2 .= "15 ";
-    };
+
+    $z = 0;
+    $res_2 = "0 -- ноль; <br>";
+    do {
+        $z++;
+        if($z % 2 == 0) {
+            $res_2 .= $z . " -- чётное число;<br>";
+        } else {
+            $res_2 .= $z . " -- нечётное число;<br>";
+        }
+    } while ($z < 10);
 
     /* /ВТОРОЕ ЗАДАНИЕ */
 
     /* ТРЕТЬЕ ЗАДАНИЕ */
+    $res_3 = '';
+    $regions = [
+        'Московская область:' => [
+            'Москва', 'Зеленоград', 'Клин'
+        ],
+        'Ленинградская область:' => [
+            'Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт'
+        ],
+        'Рязанская область:' => [
+            'Рязань', 'Касимов', 'Сасово'
+        ]
+        ];
+        foreach ($regions as $region => $cities) {
+            $res_3 .= "$region<br>";
+            for($y = 0; $y < count($cities); $y++) {
+                if($y == (count($cities) - 1)) {
+                    $res_3 .=  "$cities[$y];<br>";
+                } else {
+                    $res_3 .=  "$cities[$y], ";
+                }
+            }
+        };
 
-    function add($q = 10, $w = 20) {
-        return $q + $w;
-    };
-    function subtract($q = 10, $w = 20) {
-        return $q - $w;
-    };
-    function multiply($q = 10, $w = 20) {
-        return $q * $w;
-    };
-    function divide($q = 10, $w = 20) {
-        return $q / $w;
-    };
-
-    /* /ТРЕТЬЕ ЗАДАНИЕ */
+    /* ТРЕТЬЕ ЗАДАНИЕ */
 
     /* ЧЕТВЁРТОЕ ЗАДАНИЕ */
 
-    function mathOperation($arg1, $arg2, $operation) {
-        switch($operation) {
-            case 'add':
-                return add($arg1, $arg2);
-            case 'subtract':
-                return subtract($arg1, $arg2);
-            case 'multiply':
-                return multiply($arg1, $arg2);
-            case 'divide':
-                return divide($arg1, $arg2);
-        }
-    }
+    $letters = [
+        'a' => 'а', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo', 'ж' => 'zh', 'з' => 'z', 'и' => 'e', 'й' => 'y',
+        'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'о', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', ' ' => ' ',
+        'х' => 'h', 'ц' => 'c', 'ч' => 'ch', 'ш' => 'sh', 'щ' => "sh'", 'ь' => "'", 'э' => 'a', 'ю' => 'yu', 'я' => 'ya', 'ы' => 'y', 'ъ' => '',
+    ];
 
+    $str_4 = 'Нет фантазии';
+    $res_4 = '';
+    // foreach($letters as $key => $value) {
+    //     var_dump($key);
+    //     var_dump($value);
+    //     $res_4 = str_replace($key, $value, $str_4);
+    // };
+    foreach($letters as $key => $value) {
+        $ruLetters .= $key;
+        $enLetters .= $value;
+    };
+    var_dump($ruLetters);
+    var_dump($enLetters);
+
+    
+    for($g = 0; $g < count($letters); $g++) {
+        $res_4 = str_replace($ruLetters[$g], $enLetters[$g], $str_4);
+    };
+    
+
+    echo $res_4;
     /* /ЧЕТВЁРТОЕ ЗАДАНИЕ */
-
-    /* ШЕСТОЕ ЗАДАНИЕ */
-
-    function power($val, $pow) {
-        if ($pow == 0) {
-            return 1;
-        } elseif ($pow == 1) {
-            return $val;
-        } else {
-            return $val * power($val, $pow-1);
-        }
-    }
-
-    /* /ШЕСТОЕ ЗАДАНИЕ */
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -118,34 +100,19 @@
     <h1> <?php echo($h1) ?> </h1>
     <article>
         <h3>Первое задание</h3>
-        <p><?php echo($result_1); ?></p>
+        <p><?php echo($res_1); ?></p>
     </article>
 
     <article>
         <h3>Второе задание</h3>
-        <p><?php echo($result_2); ?></p>
+        <p><?php echo($res_2); ?></p>
     </article>
 
     <article>
         <h3>Третье задание</h3>
-        <p>Используется в четвёртом</p>
+        <p><?php echo($res_3); ?></p>
     </article>
-
-    <article>
-        <h3>Четвёртое задание</h3>
-        <p><?php echo(mathOperation(15, 10, 'divide')); ?></p>
-    </article>
-
-    <article>
-        <h3>Шестое задание</h3>
-        <p><?php echo(power(11, 2)); ?> </p>
-    </article>
-
-    <article>
-        <h3>Седьмое задание</h3>
-        <p><?php echo(date("H часов i минут")); ?> <br>
-        Суперсильно не хочу изводиться со склонениями, если подскажете относительно быстрый (в реализации) способ (при условии наличия такового) — буду благодарен</p>
-    </article>
+    
     <footer>
         <?php echo('Год: ' . date(Y)) ?>
     </footer>
