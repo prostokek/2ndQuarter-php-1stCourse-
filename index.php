94c68bfb5fd1996964da92984e9ba7d13f6bff57
prostokek <dimagalyanov@gmail.com>
2019-05-26 01:26:21 +0300

    Оба калькулятора

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ $title = 'Шестой урок';
         case 'addPic': include('pages/addPic.php'); break;
         case 'usersAddShowDelete': include('pages/usersAddShowDelete.php'); break;
         case 'gallery': include('pages/gallery.php'); break;
+        case 'calc1': include('pages/calc1.php'); break;
+        case 'calc2': include('pages/calc2.php'); break;
         default: include('pages/pageTemplate.php'); break;
     };
 
@@ -20,6 +22,8 @@ $title = 'Шестой урок';
         <li><a href="?page=gallery">Галерея</a></li>
         <li><a href="?page=usersAddShowDelete">Пользователи</a></li>
         <li><a href="?page=addPic">Добавить картинку</a></li>
+        <li><a href="?page=calc1">Калькулятор 1</a></li>
+        <li><a href="?page=calc2">Калькулятор 2</a></li>
     </ul>
 php;
 
---
+++ pages/calc2.php
@@ -0,0 +1,35 @@
+<?php
+if (isset($_POST['a']) && isset($_POST['b']) && isset($_POST['operation'])) {
+    $calc_result_addition = 'Результат сложения данных чисел: ' . ((int)$_POST['a'] + (int)$_POST['b']) . '<br>';
+    $calc_result_substraction = 'Результат вычитания данных чисел: ' . ((int)$_POST['a'] - (int)$_POST['b']) . '<br>';
+    $calc_result_multiplication = 'Результат умножения данных чисел: ' . ((int)$_POST['a'] * (int)$_POST['b']) . '<br>';
+    $calc_result_division = 'Результат деления данных чисел: ' . @((int)$_POST['a'] / (int)$_POST['b']) . '<br>';
+    switch ($_POST['operation']) {
+        case 'addition': $calc2_res = $calc_result_addition; break;
+        case 'substraction': $calc2_res = $calc_result_substraction; break;
+        case 'multiplication': $calc2_res = $calc_result_multiplication; break;
+        case 'division':  
+        if((int)$_POST['b'] === 0) {
+            $calc2_res = 'Ошибка. Вы попытались разделить на ноль';
+        } else {
+            $calc2_res = $calc_result_division;
+        }; break;
+    };
+};
+
+$content = <<<php
+    <form method = 'POST'>
+        <input type = 'text' name = 'a' placeholder = 'Первое значение'>
+        <input type = 'text' name = 'b' placeholder = 'Второе значение'>
+        <p><input type="radio" name = 'operation' value = 'addition'>Сложить</p>
+        <p><input type="radio" name = 'operation' value = 'substraction'>Вычесть</p>
+        <p><input type="radio" name = 'operation' value = 'multiplication'>Умножить</p>
+        <p><input type="radio" name = 'operation' value = 'division'>Разделить</p>
+        <input type = 'submit' value = 'Посчитать'>
+
+    </form>
+    <p>
+    {$calc2_res}
+    </p>
+php;
+?>
