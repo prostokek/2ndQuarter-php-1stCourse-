b0efa3fa7fed34b723ce96be7ed9ff21ba997d2d
prostokek <dimagalyanov@gmail.com>
2019-05-26 12:46:59 +0300

    БД с отзывами об отдельных продуктах забыл обновить

--- DBs/productsfeedback.sql
+++ DBs/productsfeedback.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 26 2019 г., 04:43
+-- Время создания: Май 26 2019 г., 12:46
 -- Версия сервера: 5.6.41
 -- Версия PHP: 5.5.38
 
@@ -42,13 +42,14 @@ CREATE TABLE `productsfeedback` (
 
 INSERT INTO `productsfeedback` (`id`, `nameOfSender`, `commentary`, `date`, `product_id`) VALUES
 (1, 'Тестер1', 'Тест1', '2019-05-26 01:22:47', 2),
-(262, 'tester8', 'test2', '2019-05-26 01:39:13', 2),
-(263, 'tester9', 'test9', '2019-05-26 01:39:38', 2),
 (264, 'tester9', 'test9', '2019-05-26 01:39:38', 2),
 (265, 'tester10', 'tester10', '2019-05-26 01:41:49', 2),
-(266, 'tester10', 'tester10', '2019-05-26 01:41:49', 2),
-(267, 'tester10', 'tester10', '2019-05-26 01:41:49', 2),
-(268, 'tester10', 'tester10', '2019-05-26 01:41:49', 2);
+(270, 'Tester1', 'test5', '2019-05-26 09:37:52', 2),
+(279, 'Tester1', 'STest1', '2019-05-26 09:39:17', 1),
+(280, 'Tester1', 'STest2', '2019-05-26 09:39:25', 1),
+(281, 'Tester1', 'STest3', '2019-05-26 09:39:32', 1),
+(282, 'Tester1', 'GTest1', '2019-05-26 09:40:58', 13),
+(283, 'Tester1', 'GTest2', '2019-05-26 09:41:04', 13);
 
 --
 -- Индексы сохранённых таблиц
@@ -68,7 +69,7 @@ ALTER TABLE `productsfeedback`
 -- AUTO_INCREMENT для таблицы `productsfeedback`
 --
 ALTER TABLE `productsfeedback`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=269;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=284;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
