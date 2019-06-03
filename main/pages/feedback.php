b9705c8aa7e3e1b50dde9568f5e5aebb054f4107
prostokek <dimagalyanov@gmail.com>
2019-06-03 17:12:49 +0300

    feedback

--- main/pages/feedback.php
+++ main/pages/feedback.php
@@ -1,34 +1,23 @@
 <?php
-$title = 'Отзывы';
-if(!empty($_POST['nameOfSender']) && !empty($_POST['commentary']) && $_POST['query'] == 'addCommentary') {
-    $nameOfSender = clearStr($_POST['nameOfSender']);
-    $commentary = clearStr($_POST['commentary']);
-
-    $sql_addCommentary = "INSERT INTO feedback(nameOfSender, commentary)
-    VALUES ('{$nameOfSender}', '{$commentary}')";
-
-    mysqli_query(connectToSQL(), $sql_addCommentary);
-    header('Location: /?page=feedback');
-};
-
-$sql_feedback = "SELECT nameOfSender, commentary, date FROM feedback ORDER BY feedback.date DESC";
-$res_feedback = mysqli_query(connectToSQL(), $sql_feedback) or die(mysqli_error(connectToSQL())); //(адрес, запрос) || получили результат запроса || or die(что делать в случае, если нет ничего по адресу)
-
-$feedbackHTML = '';
-while ($feedbackData = mysqli_fetch_assoc($res_feedback)) {
-    $feedbackDate = date('d M Y H ч. i м.', strtotime($feedbackData['date']));
-    $feedbackHTML .= <<<php
-    <h2>Имя отправителя: {$feedbackData['nameOfSender']}</h2>
-    <p>Отзыв: {$feedbackData['commentary']}</p>
-    <p>Дата: {$feedbackDate}</p>
-    <hr>
+function html() {
+    $title = 'Отзывы';
+    $sql_feedback = "SELECT nameOfSender, commentary, date FROM feedback ORDER BY feedback.date DESC";
+    $res_feedback = mysqli_query(connectToSQL(), $sql_feedback) or die(mysqli_error(connectToSQL())); //(адрес, запрос) || получили результат запроса || or die(что делать в случае, если нет ничего по адресу)
+    
+    $feedbackHTML = '';
+    while ($feedbackData = mysqli_fetch_assoc($res_feedback)) {
+        $feedbackDate = date('d M Y H ч. i м.', strtotime($feedbackData['date']));
+        $feedbackHTML .= <<<php
+        <h2>Имя отправителя: {$feedbackData['nameOfSender']}</h2>
+        <p>Отзыв: {$feedbackData['commentary']}</p>
+        <p>Дата: {$feedbackDate}</p>
+        <hr>
 php;
-};
-
-$content = <<<php
+    };
+    $content = <<<php
     <div>
         <h1>Оставить отзыв</h1>
-        <form method='POST'>
+        <form action='?page=feedback&func=addCommentary' method='POST'>
         <input type = 'text' name = 'nameOfSender' placeholder = 'Имя отправителя'>
         <textarea name = 'commentary' placeholder = 'Отзыв'></textarea>
         <input type = 'hidden' name = 'query' value = 'addCommentary'>
@@ -38,4 +27,24 @@ $content = <<<php
         {$feedbackHTML}
     </div>
 php;
-?>
+
+    $html = [
+        'content' => $content,
+        'title' => $title
+    ];
+    return $html;
+};
+
+function addCommentary() {
+    if(!empty($_POST['nameOfSender']) && !empty($_POST['commentary'])) {
+        $nameOfSender = clearStr($_POST['nameOfSender']);
+        $commentary = clearStr($_POST['commentary']);
+    
+        $sql_addCommentary = "INSERT INTO feedback(nameOfSender, commentary)
+        VALUES ('{$nameOfSender}', '{$commentary}')";
+        // echo $sql_addCommentary;
+    
+        mysqli_query(connectToSQL(), $sql_addCommentary);
+        header('Location: /?page=feedback');
+    };
+};
